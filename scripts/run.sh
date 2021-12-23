f6b5eb5bb912c7188b490e0a0bd909abe09d3b97
Yamamoto Yuki <yuki-yama@elm15.naist.jp>
2021-12-23 21:14:05 +0900

    fourth commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,13 +4,14 @@
 
 PROJ_DIR=/home/cl/yuki-yama/work/phd/v1
 SCRIPT_DIR=${PROJ_DIR}/scripts
-#MODEL_DIR=${PROJ_DIR}/torch_models
-MODEL_DIR=BERT-AMR_10_5e-05_16
+MODEL_DIR=${PROJ_DIR}/classifier/torch_models
+
+model_path=${MODEL_DIR}/BERT-AMR/10_5e-05_16/1.pth
 input=${SCRIPT_DIR}/demo.sents
 
 skele=$(
     python pipeline.py \
-        --model_name=${MODEL_DIR} \
+        --model_path=${model_path} \
         --file_path=${input}
 )
 
