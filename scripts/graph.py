a94ee7bde98566022d4ebd29cc2f8b1798bb32ce
Yamamoto Yuki <yuki-yama@elm15.naist.jp>
2021-12-25 00:03:11 +0900

    updated scripts

---
+++ scripts/graph.py
@@ -0,0 +1,29 @@
+import sys
+
+import penman
+from penman import constant
+from penman.graph import Graph
+
+from common import ROLES
+
+
+#g2 = penman.decode("(v1 / V1    :cause|condition|concession (c / consider-01        :ARG1(v2 / V2)))")
+
+def edit_skele(g, label):
+    g1 = penman.decode(g)
+    attributes = []
+    for src, role, tgt in g1.edges():
+        #print(src, role, tgt)
+        if len(role[1:].split('|')) > 1:
+            #print(True)
+            role = label
+        attributes.append((src, role, tgt))
+    g2 = penman.Graph(g1.instances() + attributes)
+    g2 = str(penman.encode(g2))
+    return g2
+
+def disamb_skele(g, pred):
+    label = ROLES[pred]
+    result = edit_skele(g, label)
+    return result
+
--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -4,6 +4,7 @@ python pipeline.py --model_path demo/model.pth --file_path demo/in.sents
 '''
 
 import os
+from posixpath import join
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -13,16 +14,17 @@ import pandas as pd
 
 import torch
 from transformers import BertForSequenceClassification
-from model import load_data, predict
 
+from graph import disamb_skele
+from model import load_data, predict
 from matcher.amr_matcher import matching
 from matcher.pattern_loader import pattern_datum_list, create_list, load_dict
+
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
-
 
 def load_model(model_path):
     model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = 4)
@@ -37,6 +39,14 @@ def load_sents(file_path):
         sents = [sent.rstrip() for sent in sents]
     return sents
 
+class DepMatch:
+    def __init__(self):
+        self.sent = ''
+        self.skeles = []
+        self.clauses = []
+        self.ambs = []
+        self.results = []
+
 def run_matcher(sents):
     pd_path = '../matcher/pattern_dict'
     pd_list = pattern_datum_list(pd_path)
@@ -45,31 +55,29 @@ def run_matcher(sents):
     print('Loaded Pattern Dictionary\n')
     print('Start Dependency Matching\n')
 
-    skele_list, conll_list, clause_pair_list, amb_list = [], [], [], []
+    conll_list = []
+    ##result = [doc, [skele1, skele2], [(SUB, MAT, SCONJ), (SUB, MAT, SCONJ)], [F, T]]
 
-    for sent in sents:
+    d_list = []
+    for i,sent in enumerate(sents):
         results, doc, conll, _ = matching(sent)
         tok_list = [t.text for t in doc]
-        #print(tok_list)
 
-        tmp_s, tmp_c, tmp_a = [], [], []
+        d = DepMatch()
+        d.sent = doc
         if results:
-            tmp_s.append(sent)
             conll_list.append(conll)
-            for i,result in enumerate(results):
-                #print(result)
-                matched_id = results[i][0]
+            for j,result in enumerate(results):
+                matched_id = results[j][0]
                 skeleton = amr_list[matched_id+1]
                 skele_id = id_list[matched_id]
-                print(skele_id)
                 sp = skele_id.split(".")
                 if sp[-1] == '*': ##if ambiguous
-                    tmp_a.append(True)
+                    d.ambs.append(True)
                     if int(sp[0])==1:
                         ##result = (0, [['went', 'I', 'ate', 'I', 'after']], [[1, 0, 7, 6, 5]])
                         tree_match_id = result[2][0]
                         sconj_i, matv_i, subv_i = tree_match_id[-1], tree_match_id[0], tree_match_id[2]
-
                         #print('subtree')
                         mat_stree = [t for t in doc[matv_i].subtree]
                         sub_stree = [t for t in doc[subv_i].subtree]
@@ -88,40 +96,27 @@ def run_matcher(sents):
                                 matrix_tok = matrix_tok[:-1]
                             matrix = ' '.join(matrix_tok)
                             clause_pair = (subord, matrix, tok_list[sconj_i])
-                        tmp_c.append(clause_pair)
+                        d.clauses.append(clause_pair)
                         
                     else:
                         sys.exit('to be implemented')
                 else:
-                    tmp_a.append(False)
-                    tmp_c.append(('-','-','-'))
+                    d.ambs.append(False)
+                    d.clauses.append(('-','-','-'))
                 skele = ''.join(skeleton)
-                #print(skele + '\n')
-                tmp_s.append(skele)
-                
-        skele_list.append(tmp_s)
-        clause_pair_list.append(tmp_c)
-        amb_list.append(tmp_a)
-    
-    ## skele_list = [[sent, skele1, skele2], [sent, skele1]]
-    ## clause_pair_list = [[(SUB, MAT, SCONJ), (SUB, MAT, SCONJ)], [(SUB, MAT, SCONJ)]]
-    ## amb_list = [[F, T], [T]]
-    return skele_list, clause_pair_list, amb_list
+                d.skeles.append(skele)
+        d_list.append(d)
 
-def disambiguate(model, skele_list, clause_pair_list, amb_list):
+    return d_list
 
-    final_results = []
+def disambiguate(model, d_list):
+
+    ## make input
     amb_cp_list = []
-
-    for i,(skeles,clause_pair, amb) in enumerate(zip(skele_list, clause_pair_list, amb_list)):
-        sent = skeles[0]
-        skel = skeles[1:]
-        for i,s in enumerate(skel):
-            if amb[i] == True:
-                print('True')
-                amb_cp_list.append(clause_pair[0])
-            else:
-                print('False')
+    for i,d in enumerate(d_list):
+        for j,amb in enumerate(d.ambs):
+            if amb == True:
+                amb_cp_list.append(d.clauses[j])
 
     disamb_path = 'tmp.csv'
     with open(disamb_path, 'w') as f:
@@ -129,20 +124,31 @@ def disambiguate(model, skele_list, clause_pair_list, amb_list):
         for amb_cp in amb_cp_list:
             writer.writerow(amb_cp)
 
+    ## role classification
     dataloader = load_data(disamb_path, s=False, batch_size=1)
     predictions = predict(dataloader, model, device, o=True)
-
-    print(predictions)
     
-    return final_results
+    ## provide resulting skeletons
+    cnt=0
+    for i,d in enumerate(d_list):
+        for j,amb in enumerate(d.ambs):
+            if amb == True:
+                primitive = d.skeles[j]
+                result = disamb_skele(primitive, predictions[cnt])
+                d.results.append(result)
+                cnt+=1
+            else:
+                d.results.append(d.skeles[j])
 
-def write_results(results):
-    out_path='out.skele'
+    return d_list
+
+def write_results(d_list, out_path):
+
     with open(out_path, mode='w', encoding='utf-8') as o:
-        for i,result in enumerate(results):
-            o.write('#SENT '+str(i)+'\n')
-            for r in result:
-                o.write('#SKELE'+'\n')
+        for i,d in enumerate(d_list):
+            o.write('#SENT '+str(d.sent)+'\n')
+            for j,r in enumerate(d.results):
+                o.write('#SKELE'+str(j)+'\n')
                 o.write(r+'\n')
             o.write('\n')
 
@@ -157,21 +163,18 @@ def main():
     sents = load_sents(file_path)
 
     print('###DEPENDENCY MATCHING PHASE###\n')
-    skele_list, clause_pair_list, amb_list = run_matcher(sents)
-    print(skele_list)
-    print(clause_pair_list)
-    print(amb_list)
+    d_list = run_matcher(sents)
 
     print('###SEMANTIC DISAMBIGUATION PHASE###\n')
     print('Loading: '+str(model_path))
     model = load_model(model_path)
+    d_list = disambiguate(model, d_list)
+    for d in d_list:
+        print(d.results)
 
-    results = disambiguate(model, skele_list, clause_pair_list, amb_list)
-
-    '''
-    write_results(results)
-    return(results)
-    '''
+    print('###WRITING OUT RESULTS###')
+    out_path='demo/out.skele'
+    write_results(d_list, out_path)
 
 if __name__ == '__main__':
     main()
